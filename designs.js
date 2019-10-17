7369266f4b77c299b35d479482a8a6125472b384
Sara Eisa <prosarahgamal@outlook.com>
2019-10-17 22:10:09 +0200

    feat: add event listeners to store values in app state
    add change and key up event listener for inputs

--- designs.js
+++ designs.js
@@ -1,5 +1,21 @@
 const appState = {
-  "color": "#000000",
+  "color": "#000002",
   "width": 1,
   "height": 1
 };
+
+const makeGrid = (target) => {
+  
+}
+
+const setColor = (target) => {
+  appState.color = target.value
+}
+
+const setWidth = (target) => {
+  appState.width = +target.value
+}
+
+const setHeight = (target) => {
+  appState.height = +target.value
+}
